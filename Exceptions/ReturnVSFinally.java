a10741995e2e1b7e306b15496a84afa202545a24
Dasharath Manda <dasharath.m@gmail.com>
2018-07-02 16:18:15 +0530

    Update ReturnVSFinally.java

--- Exceptions/ReturnVSFinally.java
+++ Exceptions/ReturnVSFinally.java
@@ -1,25 +1,18 @@
 package com.dasharth.exceptions;
 
-<!-- Even though return present in try or catch blocks first finally will be executed ,
-and after that only return statement will be considered that is finally block dominates return statement. -->
+//Even though return present in try or catch blocks first finally will be executed ,
+//and after that only return statement will be considered that is finally block dominates return statement. 
 
-class ReturnVSFinally
-{
-public static void main(String[] args)
-{
-try
-{
-System.out.println("try block executed");
-return;
+class ReturnVSFinally {
+    public static void main(String[] args) {
+        try {
+            System.out.println("try block executed");
+            return;
+        } catch (ArithmeticException e) {
+            System.out.println("catch block executed");
+        } finally {
+            System.out.println("finally block executed");
+        }
+    }
 }
-catch(ArithmeticException e)
-{
-System.out.println("catch block executed");
-}
-finally
-{
-System.out.println("finally block executed");
-}}}
-Output:
-Try block executed
-Finally block executed
+
