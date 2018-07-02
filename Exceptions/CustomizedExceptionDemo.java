85dfaa2eca8b99d231dffcd4c5769ca73d9c34d9
Dasharath Manda <dasharath.m@gmail.com>
2018-07-02 16:40:37 +0530

    Customized Exception
    Sometimes we can create our own exception to meet our programming requirements. Such type of exceptions are called customized exceptions (user defined exceptions).

---
+++ Exceptions/CustomizedExceptionDemo.java
@@ -0,0 +1,26 @@
+package com.journaldev;
+
+class TooYoungException extends RuntimeException {
+    TooYoungException(String s) {
+        super(s);
+    }
+}
+
+class TooOldException extends RuntimeException {
+    TooOldException(String s) {
+        super(s);
+    }
+}
+
+class CustomizedExceptionDemo {
+    public static void main(String[] args) {
+        int age = 30; //Integer.parseInt(args[0]);
+        if (age > 60) {
+            throw new TooYoungException("please wait some more time.... u will get best match");
+        } else if (age < 18) {
+            throw new TooOldException("u r age already crossed....no chance of getting married");
+        } else {
+            System.out.println("you will get match details soon by e-mail");
+        }
+    }
+}
